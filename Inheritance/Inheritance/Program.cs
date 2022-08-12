cff963e059220de646eea06975e8eb5a89c875de
shubham <shubham.jha@kalpitatechnologies.com>
2022-08-12 14:13:11 +0530

    First Assignment

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Animal
+    {
+
+        public string? name;
+
+        public void display()
+        {
+            Console.WriteLine("animal");
+        }
+
+    }
+    class Dog : Animal
+    {
+
+        public void getName()
+        {
+            Console.WriteLine("" + name);
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Dog labrador = new Dog();
+            labrador.name = "puppy";
+            labrador.display();
+            labrador.getName();
+
+            Console.ReadLine();
+        }
+
+    }
+}
